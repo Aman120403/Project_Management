e42ea7815058188f0343ca5ac093d3712ad7a0cc
Aman120403 <amanmaurya904463@gmail.com>
2026-04-07 21:28:37 +0530

    adding api error-response and connecting db

--- src/index.js
+++ src/index.js
@@ -1,20 +1,24 @@
 import dotenv from "dotenv";
-import express from "express";
+import app from "./app.js"
+
+import connectDB from "./db/index.js"
 dotenv.config({
     path:"./.env",
 });
 
 
-const app = express()
+
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-app.get("/instagram", (req,res) =>{
-    res.send("this is a an instagram page");
-})
 
-app.listen(port, () => {
-  console.log(`Example app listening on port http://localhost:${port}`)
+connectDB()
+.then(()=>{
+    app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`);
+});
+
 })
+.catch((err)=>{
+    console.error("MongoDB connection failed");
+    process.exit(1);
+});
