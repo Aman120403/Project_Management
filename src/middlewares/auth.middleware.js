fde9a559d0305e221e1b8295718867c8570251c6
Aman120403 <amanmaurya904463@gmail.com>
2026-04-14 21:12:48 +0530

    routes has been added

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -4,10 +4,10 @@ import {asyncHandler} from "../utils/async-handler.js"
 import jwt from "jsonwebtoken"
 
 export const verifyJWT = asyncHandler(async(req,res,next)=>{
-    const token = res.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "");
+    const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "");
     
     if(!token){
-        throw new ApiError(401, "Unauthorizes request");
+        throw new ApiError(401, "Unauthorized request");
     }
     try {
         const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
